769b3175c139d55a9950b58508838e2a578a8cd4
adil <adilaitelhoucine1@gmail.com>
2024-09-19 17:22:28 +0200

    tasks done : connecxion,inscription,validation des champs,menus

--- Main.c
+++ Main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX 100
+#define MAX_USERS 100
+#define MAX_CLAIMS 100
 
 typedef struct {
     int id;
@@ -13,10 +14,40 @@ typedef struct {
     char category[50];
     char status[20];
     char date[20];
-} recla;
+} reclamation;
 
-recla claims[MAX];
+typedef struct {
+    char username[20];
+    char password[20];
+    char email[50];
+    char role[10]; // "admin", "agent" ou "client"
+} User;
+
+User users[MAX_USERS];
+int userCount = 0;
+reclamation claims[MAX_CLAIMS];
 int claimCount = 0;
+
+int ValiderNom(char *nom) {
+    for (int i = 0; i < strlen(nom); i++) {
+        if (!isalpha(nom[i]) && nom[i] != ' ') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int ValiderMail(char *mail) {
+    const char *texte = mail;
+    char *ars = strchr(texte, '@');
+    if (ars != NULL) {
+        char *dot = strchr(ars, '.');
+        if (dot != NULL) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int validatePassword(char *username, char *password) {
     int upper = 0, lower = 0, digit = 0, special = 0;
@@ -35,29 +66,51 @@ int validatePassword(char *username, char *password) {
     }
 }
 
-void signup() {
-    char username[20], password[20];
-    printf("enter votre username: ");
-    gets(username);
+void formulaireInscription() {
+    User newuser;
 
-    printf("enter mot de pass: ");
-    gets(password);
+    printf("\n-----------Creation de compte-----------\n");
+    printf("Entrez votre nom d'utilisateur: ");
+    scanf(" %[^\n]", newuser.username);
 
-    if (validatePassword(username, password)) {
-        //saveUser(username, password);
-        printf("compte a est cree\n");
+    if (!ValiderNom(newuser.username)) {
+        printf("Erreur : Le nom doit contenir des alphabets uniquement.\n");
+        return;
+    }
+
+    printf("Entrez votre email : ");
+    scanf("%s", newuser.email);
+
+    if (!ValiderMail(newuser.email)) {
+        printf("Erreur : L'email n'est pas valide (doit contenir @ et .).\n");
+        return;
+    }
+
+    printf("Entrez votre mot de passe: ");
+    scanf("%s", newuser.password);
+
+    if (!validatePassword(newuser.username, newuser.password)) {
+        printf("Erreur : Le mot de passe ne repond pas aux criteres requis.\n");
+        return;
+    }
+
+    // Ajouter l'utilisateur au tableau
+    if (userCount < MAX_USERS) {
+        users[userCount] = newuser; // Ajoute le nouvel utilisateur
+        printf("Vous avez cree votre compte avec succes.\n");
+        userCount++;
     } else {
-        printf("mot de passe ne contient pas les requises\n");
+        printf("Erreur : Limite d'utilisateurs atteinte.\n");
     }
 }
+
 int signinAsAdmin() {
     char username[20], password[20];
-    printf("veuillez entrer Admin username: ");
-    scanf(" %[^\n]",username);
+    printf("Veuillez entrer le nom d'utilisateur Admin: ");
+    scanf("%s", username);
 
-    printf("veuillez entrer Admin password: ");
-    scanf(" %[^\n]",password);
-
+    printf("Veuillez entrer le mot de passe Admin: ");
+    scanf("%s", password);
 
     if (strcmp(username, "admin") == 0 && strcmp(password, "admin@123") == 0) {
         printf("Connexion admin reussie\n");
@@ -66,60 +119,155 @@ int signinAsAdmin() {
         return 0;
     }
 }
+
 int signinAsClient() {
     char username[20], password[20];
-    printf("veuillez entrer Client username : ");
-    gets(username);
-    printf("veuillez entrer Client mot de pass : ");
-    gets(password);
+    printf("Veuillez entrer le nom d'utilisateur Client: ");
+    scanf(" %[^\n]", username);
 
+    printf("Veuillez entrer le mot de passe Client: ");
+    scanf("%s", password);
 
-        printf("connexion Client reussie\n");
-        return 1;
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+            printf("Connexion Client réussie\n");
+            return 1;
+        }
+    }
 
+    printf("Echec de la connexion en tant que client\n");
+    return 0;
 }
+
+ void connexion(){
+     int trouve=0;
+    char username[20], password[20];
+    printf("\n-----------Connexion-----------\n");
+
+    printf("Veuillez entrer le nom d'utilisateur : ");
+    scanf(" %[^\n]", username);
+
+    printf("Veuillez entrer le mot de passe : ");
+    scanf("%s", password);
+
+    int i;
+    for(i=0;i<userCount;i++){
+     if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+           trouve=1;
+           break;  // Utilisateur trouvé
+        }
+    }
+
+    if(trouve==1){
+
+        clientMenu();
+    }else{
+        printf("Connexion a Echoouee");
+    }
+ }
+ void clientMenu(){
+  int choice;
+    do {
+        printf("\n========== Client Menu ==========\n");
+        printf("1. Add Claim\n");
+        printf("2. View My Claims\n");
+        printf("3. Modify My Claim\n");
+        printf("4. Delete My Claim\n");
+        printf("0. Logout\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1:
+             //   addClaim(clientName);
+                break;
+            case 2:
+               // viewMyClaims(clientName);
+                break;
+            case 3:
+               // modifyMyClaim(clientName);
+                break;
+            case 4:
+                //deleteMyClaim(clientName);
+                break;
+            case 0:
+                printf("Logging out...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 0);
+}
+ void adminMenu(){
+  int choice;
+    do {
+        printf("\n========== Admin Menu ==========\n");
+        printf("1. View All Claims\n");
+        printf("2. Process Claim\n");
+        printf("3. Add New Client\n");
+        printf("4. Add/Delete Agent Role\n");
+        printf("0. Logout\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+
+                break;
+            case 0:
+                printf("Logging out as Admin...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 0);
+}
+
+
 int main() {
     int choice;
-
+int i;
     do {
         printf("\n============================MENU============================\n");
-        printf("1. Connectez-vous en tant qu'admin\n");
-        printf("2. Connectez-vous en tant qu'agent\n");
-        printf("3. Connectez-vous en tant qu'clien\n");
+        printf("1. Espace admin\n");
+        printf("2. Inscription Client\n");
+        printf("3. Connexion Client\n");
+        printf("4. Connexion Agent\n");
         printf("0. Exit ...\n");
-        printf("veuillez entrer votre choice: ");
-        scanf("%d",&choice);
+        printf("Veuillez entrer votre choix: ");
+        scanf("%d", &choice);
 
         switch (choice) {
             case 1:
                 if (signinAsAdmin()) {
-                    //adminMenu();
+                    adminMenu();
                 } else {
-                    printf("echec de la connexion en tant qu'admin\n");
+                    printf("Echec de la connexion en tant qu admin\n");
                 }
                 break;
             case 2:
-                /*if (signinAsAgent()) {
-                   // agentMenu();
-                } else {
-                printf("echec de la connexion en tant qu'agent\n");
-                }
-                break;*/
+                formulaireInscription();
+                break;
             case 3:
-                /*if (signinAsClient()) {
-                    char clientName[20];
-                    printf("veuillez entrer votre username: ");
-                    gets(clientName);
-                  //  clientMenu(clientName);
-                } else {
-                    printf("echec de la connexion en tant qu'clien\n");
-                }
-                break;*/
+                    connexion();
+                break;
             case 0:
-               /* printf("Exiting program ...\n");
-                brea*/;
+                printf("Merci d utiliser notre App !!");
+                break;
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("Choix invalide. Veuillez réessayer.\n");
         }
     } while (choice != 0);
 
